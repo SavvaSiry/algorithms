928fb3ac28239c43100f6ce056ab2a3550dba838
Savva <savvasiry@gmail.com>
2023-03-23 22:34:00 +0300

    init

---
+++ src/Brackets.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Brackets {
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(reader.readLine());
+            generate("", 0, 0, n);
+        }
+    }
+
+    public static void generate(String cur, int open, int close, int n) {
+        if (cur.length() == n * 2) {
+            System.out.println(cur);
+            return;
+        }
+        if (open < n) {
+            generate(cur + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            generate(cur + ")", open, close + 1, n);
+        }
+    }
+
+}
---
+++ src/VectorLength.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class VectorLength {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String vectorStr = reader.readLine();
+            String[] vector = vectorStr.split(" ");
+            int localLength = 0, maxLength = 0;
+            for (int i = 0; i < vector.length; i++) {
+                if (Objects.equals(vector[i], "1")) {
+                    localLength++;
+                    if (localLength > maxLength) maxLength = localLength;
+                } else localLength = 0;
+            }
+            System.out.println(maxLength);
+        }
+    }
+}
