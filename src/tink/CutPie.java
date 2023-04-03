8821748f44bfe853af65cb9efa7c6a9f8445f9c7
Savva <some@mail.ru>
2023-04-03 18:12:41 +0300

    feat(tink_CutPie): done

---
+++ src/tink/CutPie.java
@@ -0,0 +1,16 @@
+package tink;
+
+import java.util.Scanner;
+
+public class CutPie {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int counter = 0;
+        while (n != 1) {
+            n = n/2 + n % 2;
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
