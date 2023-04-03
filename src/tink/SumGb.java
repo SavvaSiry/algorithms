ad758f862172f05e9c950bb1f0ce8047bf41b73b
Savva <some@mail.ru>
2023-04-03 18:13:05 +0300

    feat(tink_SumGb): done

---
+++ src/tink/SumGb.java
@@ -0,0 +1,15 @@
+package tink;
+
+import java.util.Scanner;
+
+public class SumGb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        if (d > b) System.out.println(a + (d - b) * c);
+        else System.out.println(a);
+    }
+}
