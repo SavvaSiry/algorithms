e6f91738ab44460605c1097b60c23d54b2c55e78
Savva <savvasiry@gmail.com>
2023-04-10 22:56:39 +0300

    feat(Equation): done

--- src/contestv1/Equation.java
+++ src/contestv1/Equation.java
@@ -8,8 +8,14 @@ public class Equation {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        if (c < 0 || (c * c - b) % a != 0 || (c * c - b) / a < 0) System.out.println("NO SOLUTION");
-        else if (a == 0) System.out.println("MANY SOLUTIONS");
-        else System.out.println((c * c - b) / a);
+
+        if (c < 0)
+            System.out.println("NO SOLUTION");
+        else if (a == 0 && b == 0 && c == 0 || (a == 0 && c * c == b)) System.out.println("MANY SOLUTIONS");
+        else {
+            if (a != 0 && (c * c - b) * 1.0 / a % 1 == 0) System.out.println((c * c - b) / a);
+            else System.out.println("NO SOLUTION");
+
+        }
     }
 }
