85e42727c70303ab02b3e51407006f2f8b7760b8
Savva <savvasiry@gmail.com>
2023-04-12 01:12:35 +0300

    refactor(NearestNeighbors): only Stream API

--- src/contestv1/hw2/NearestNeighbors.java
+++ src/contestv1/hw2/NearestNeighbors.java
@@ -8,17 +8,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class NearestNeighbors {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] numbers = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] numbers = Arrays.stream(reader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        int counter = 0;
-        for (int i = 1; i < numbers.length - 1; i++) {
-            if ( numbers[i - 1]< numbers[i] &&  numbers[i] > numbers[i + 1]) counter++;
-        }
-        System.out.println(counter);
+        System.out.println(IntStream
+                .range(1, numbers.length - 1)
+                .filter(i -> numbers[i - 1] < numbers[i] &&  numbers[i] > numbers[i + 1]).count());
+
     }
 }
